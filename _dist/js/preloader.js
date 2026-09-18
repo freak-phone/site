8640dd4629c7db2cb584f77dbe917799dbab4f8a
luc <mwdev8@yahoo.com>
2026-09-17 19:43:22 -0500

    changes

--- _dist/js/preloader.js
+++ _dist/js/preloader.js
@@ -1,7 +1,7 @@
 // makes sure the whole site is loaded
 $(window).load(function () {
   // will first fade out the loading animation
-  $("#status").delay(200).fadeOut("slow");
+  $("#status").delay(200).fadeOut("fast");
   // will fade out the whole DIV that covers the website.
-  $("#preloader").delay(1000).fadeOut("fast");
+  $("#preloader").delay(600).fadeOut("fast");
 })
